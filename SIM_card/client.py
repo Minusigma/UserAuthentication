6641074d2df69e7721b92c966113cab0fc2b9ccc
Minusigma <535273272@qq.com>
2024-11-25 17:56:47 +0800

    modify server client and functions

--- SIM_card/client.py
+++ SIM_card/client.py
@@ -1,1 +1,23 @@
 from functions import *
+import time
+
+if __name__ == '__main__':
+    print("Simulation started.")
+    established_client, info = tcp_conn()
+    print(info)
+
+    if established_client:
+        device_num = create_device(established_client)
+        while True:
+            cmd = input().strip()
+            if not cmd:
+                continue
+            established_client.send(cmd)
+
+            recv_data = established_client.recv(2048)
+
+            print(f"Server:\n{recv_data}")
+            if recv_data == 'disconnected':
+                break
+        established_client.close()
+    print('Goodbye')
--- SIM_card/functions.py
+++ SIM_card/functions.py
@@ -8,3 +8,45 @@ import ast
 import fileinput
 import time
 import psycopg2
+
+SERVER_IP = 'localhost'
+SERVER_PORT = '5555'
+
+def conn_db():
+    pass
+
+def close_db(db):
+    db.close()
+    print('Database disconnected.')
+
+def execute_sql(sql):
+    pass
+
+def create_device(conn):
+    device_num = random.randint(100, 1000)
+    conn.send(f"createdevice {device_num}")
+    conn.recv(2048)
+    return device_num
+
+def device_add(db_conn, d_num):
+    sql = """INSERT INTO device_info (device_num, phone_num) VALUES (%d, %d)"""
+    param = (d_num , None)
+    cur = db_conn.cursor()
+    cur.execute(sql,param)
+    db_conn.conmmit()
+    feedback = 'Add succeed.'
+    return d_num, feedback    
+
+
+def tcp_conn():
+    ip,port = SERVER_IP, SERVER_PORT
+    port = (int) (port)
+    information = 'success'
+    socket_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        socket_client.connect((ip,port))
+    except socket.error as e:
+        information = (str)(e)
+    return socket_client,information
+
+def get_phone_num(db_conn, )
