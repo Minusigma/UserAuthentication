4e97ad08ae612d36f3c6a2b6d9d5638ce65419b5
Minusigma <535273272@qq.com>
2024-11-26 18:38:14 +0800

    333

--- SIM_card/functions.py
+++ SIM_card/functions.py
@@ -14,3 +14,28 @@ USER = "admin"
 PASSWORD = "123456"
 HOST = "localhost"
 PORT = "5432"
+
+def conn_db():
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER, 
+                            password=PASSWORD, 
+                            host=HOST, 
+                            port=PORT)
+    
+    # cur = conn.cursor()
+    # cur.execute('''CREATE TABLE device_info(
+    #            DeviceNum INT PRIMARY KEY NOT NULL,
+    #            PhoneNum INT NOT NULL)''')
+    # conn.commit()
+    print("Connect Succeed.")
+    # conn.close()
+    return conn
+
+def close_db(conn):
+    conn.close()
+    print("Connection closed.")
+
+def execute_sql(conn, cmd):
+    cur = conn.cursor()
+    cur.execute(cmd)
+    print("Executed succeed.")
